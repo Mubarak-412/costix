e4200ab4eb42c9cf52f6d219511efabc0d3bb695
Mubarak-412 <mubarak@finopsly.com>
2025-09-02 11:22:52 +0530

    disable ssr

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from gradio_ui.app import demo
 
 
 if __name__ == '__main__':
-    demo.launch(server_name='0.0.0.0',server_port=7860)
+    demo.launch(server_name='0.0.0.0',server_port=7860,ssr=False)
