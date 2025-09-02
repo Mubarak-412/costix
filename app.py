a8382d8e619337f7fe0eb136439b3a8005470966
Mubarak-412 <mubarak@finopsly.com>
2025-09-02 11:24:55 +0530

    dissable ssr

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from gradio_ui.app import demo
 
 
 if __name__ == '__main__':
-    demo.launch(server_name='0.0.0.0',server_port=7860,ssr=False)
+    demo.launch(server_name='0.0.0.0',server_port=7860,ssr_mode=False)
